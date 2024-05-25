0cdd7e295721e214ebd639b1be65cd7d0884aab6
Yalloma100 <164032163+Yalloma100@users.noreply.github.com>
2024-05-25 15:28:50 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -796,7 +796,18 @@ def promter(message):
 keep_alive()
 
 
+# main.py
+import streamlit as st
 
+def main():
+    st.title("Мій перший додаток Streamlit")
+    user_name = st.text_input("Як вас звати?")
+    if user_name:
+        st.write(f"Привіт, {user_name}!")
+
+if __name__ == "__main__":
+    main()
+  
 
 while True:
   try:
