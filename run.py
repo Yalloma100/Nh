b28961635b40c45a86ac3aeb6bdae17ef2ee9ec2
Yalloma100 <164032163+Yalloma100@users.noreply.github.com>
2024-05-25 15:20:15 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import subprocess
 
 # Замініть "file1.py" та "file2.py" на імена ваших файлів Python
-subprocess.run(["python", "file1.py"])
-subprocess.run(["python", "file2.py"])
+subprocess.run(["python", "Bot.py"])
+subprocess.run(["python", "main.py"])
